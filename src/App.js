dd7e9071e17cacc413cd05f0eec55484b6b72eac
Shivam <shivamhande0000@gmail.com>
2021-10-08 21:54:22 +0530

    app files add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/navbar.component';
+import Home from './Home';
+import Landing from './landing';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Router>
+           <div className="App">
+            <Switch>
+              <Route exact path='/' component={Landing}></Route>
+              <Route exact path='/home' component={Home}></Route>
+            </Switch>
+          </div>
+       </Router>
     </div>
   );
 }
